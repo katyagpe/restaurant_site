c7f7b40bb867c675f7319d68ea7a8abff0379614
Katya Salas <k.salas@efisense.com>
2021-09-28 06:43:09 -0500

    sistema de restaurante

---
+++ application/views/authentication/userProfile.php
@@ -0,0 +1,52 @@
+ <style type="text/css">
+    .user_detail_container{
+        background-color: #fff;
+        border-radius: 5px;
+        color: #3c8dbc;
+        padding: 5px 5px 5px 15px; 
+        min-height: 362px;
+    }
+
+    .user_info_header{
+        background-color: #3c8dbc;
+        border-radius: 5px;
+        color: white;
+        padding: 5px 5px 5px 15px; 
+        margin-top: 0px; 
+    }  
+    .box.box-primary {
+        border-top-color: #ffffff;
+        border: 1px solid #3c8dbc;
+    }
+</style>
+
+<!-- Main content -->
+<section class="content"> 
+    <!-- general form elements -->
+    <div class="box box-primary">
+
+        <div class="box-body">
+            <div class="row"> 
+                <div class="col-md-4">
+                    <!-- <img src="<? /*php echo base_url(); */?>assets/images/nacode.png" alt="" class="img-rounded img-responsive" />-->
+                </div>
+                <div class="hidden-lg">&nbsp;</div>
+                <div class="col-md-8">
+                    <h1 class="user_info_header"><h2>SolucionDigital.net</h2></h1> 
+                    <p> * Sistema de Ventas Multisucursal</p>
+                   <p>* Sistema Pos Contable y Recursos Humanos</p>
+                   <p>* Sistema de Gestion Hotelera</p>
+                    
+                   <p> *  Sistema de Farmacias</p>
+                    <p>* Sistemas de Gestion Educativa</p>
+                    
+                    <p>* Sistema para Restaurantes </p>
+                     <p>* Sistema para prestamos </p>     
+                     <a href="https://soluciondigital.net/" class="btn btn-success">Ir a Solucion Digital</a> 
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section> 
